61ce8444b54d7594500aef5cd974f69f10e5fe20
sly01 <aerkoc01@gmail.com>
2019-10-25 20:54:23 +0200

    create_store endpoint created

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -17,7 +17,13 @@ stores = [
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
